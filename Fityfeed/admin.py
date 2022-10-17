0aca04fa695518edf8d95234104f88a510948043
Naptoss <tomgabinio@gmail.com>
2022-10-17 13:12:41 -0300

    .

---
+++ Fityfeed/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import *
+
+
+class foodAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Fooditem
+    list_display = ['name']
+    list_filter = ['name']
+
+
+admin.site.register(Customer)
+admin.site.register(UserFoodItem)
+admin.site.register(Category)
+admin.site.register(Fooditem, foodAdmin)
---
+++ Fityfeed/filters.py
@@ -0,0 +1,9 @@
+from dataclasses import field
+import django_filters
+from .models import *
+
+
+class fooditemFilter(django_filters.FilterSet):
+    class Meta:
+        model = Fooditem
+        field = ['name']
